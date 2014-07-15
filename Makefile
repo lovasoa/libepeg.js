2c37fc9487ea1947db458e44fe1f52e6a2bcebb3
ophir <ophir.lojkine@af83.com>
2014-07-15 17:27:53 +0200

    Make it work in a web worker

--- Makefile
+++ Makefile
@@ -6,6 +6,6 @@ libepeg.js: libepeg/libepeg.o libjpeg/build-emscripten/.libs/libjpeg.so
 	emcc $(EXPORTS) $^ --post-js js/api.js -o libepeg.js
 
 epeg.js:  js/post.js libepeg/libepeg.o epeg/epeg.o libjpeg/build-emscripten/.libs/libjpeg.so  
-	emcc -O3 -s TOTAL_MEMORY=60000000 -s EXPORTED_FUNCTIONS="['_create_thumbnail']" libepeg/libepeg.o epeg/epeg.o libjpeg/build-emscripten/.libs/libjpeg.so --post-js js/post.js -o epeg.js
+	emcc -O3 -s TOTAL_MEMORY=67108864 -s EXPORTED_FUNCTIONS="['_create_thumbnail']" libepeg/libepeg.o epeg/epeg.o libjpeg/build-emscripten/.libs/libjpeg.so --post-js js/post.js -o epeg.js
 
 all: epeg.js
--- js/post.js
+++ js/post.js
@@ -1,9 +1,20 @@
 var native_func = Module.cwrap('create_thumbnail', 'number', ['number', 'number', 'number', 'number', 'number', 'number', 'number']);
 var tmpPointer = Runtime.stackAlloc(4);
 
-function create_thumbnail(jpegData, maxWidthHeight) {
-  inputPointer = allocate(jpegData, 'i8', ALLOC_NORMAL);
-  size = native_func(inputPointer, jpegData.length, tmpPointer, 0,0, 100, 70);
+function create_thumbnail(jpg, w, h, maxwh, q) {
+  if (!jpg) throw "No input image";
+  if (!(maxwh || (w && h))) throw "No dimensions";
+  if (!q) q = 80; // quality
+  if (typeof w === 'string' && w[w.length-1] === '%') {
+    // width given as a percentage
+    w = -Math.round(parseFloat(w));
+  }
+  if (typeof h === 'string' && h[h.length-1] === '%') {
+    // height given as a percentage
+    h = -Math.round(parseFloat(h));
+  }
+  inputPointer = allocate(jpg, 'i8', ALLOC_NORMAL);
+  size = native_func(inputPointer, jpg.length, tmpPointer, w, h, maxwh, q);
   Module._free(inputPointer);
   if (size === 0) {
     throw "Native function error";
@@ -14,4 +25,22 @@ function create_thumbnail(jpegData, maxWidthHeight) {
     thumb[i] = Module.HEAPU8[begin + i];
   }
   return thumb;
+}
+
+if (typeof importScripts !== 'undefined') {
+  // web worker
+  self.onmessage = function (event) {
+    var data = event['data'];
+    var jpg = data['jpegData'],
+        w   = data['width'],
+        h   = data['height'],
+        maxwh = data['maxWidthHeight'],
+        q   = data['quality'],
+        id  = data['id'];
+    var thumb = create_thumbnail(jpg, w, h, maxwh, q);
+    postMessage({
+      'id'       : id,
+      'thumbnail': thumb
+    }, [thumb.buffer]);
+  }
 }
